8b430fac58632bb9cbe5c7db9acf02cc97e17fa5
David M <damoba.dev@gmail.com>
2025-03-14 02:07:33 -0400

    reorder custom plugins

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -23,26 +23,25 @@ require('lazy').setup({
   -- modular approach: using `require 'path/name'` will
   -- include a plugin definition from file lua/path/name.lua
 
-  require 'kickstart/plugins/gitsigns',
+  require('kickstart/plugins/gitsigns'),
 
-  require 'kickstart/plugins/which-key',
+  require('kickstart/plugins/which-key'),
 
-  require 'kickstart/plugins/telescope',
+  require('kickstart/plugins/telescope'),
 
-  require 'kickstart/plugins/lspconfig',
+  require('kickstart/plugins/lspconfig'),
 
-  require 'kickstart/plugins/conform',
+  require('kickstart/plugins/conform'),
 
-  require 'kickstart/plugins/cmp',
+  require('kickstart/plugins/cmp'),
 
   -- require 'kickstart/plugins/tokyonight',
-  require 'kickstart/plugins/gruber-darker',
 
-  require 'kickstart/plugins/todo-comments',
+  require('kickstart/plugins/todo-comments'),
 
   -- require 'kickstart/plugins/mini',
 
-  require 'kickstart/plugins/treesitter',
+  require('kickstart/plugins/treesitter'),
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
@@ -57,13 +56,13 @@ require('lazy').setup({
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
-  require 'kickstart.plugins.neo-tree',
+  require('kickstart.plugins.neo-tree'),
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
