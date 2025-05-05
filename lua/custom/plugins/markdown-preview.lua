831099a371263d3da5ac24227ae7c09d4c1bcc6c
David M <damoba.dev@gmail.com>
2025-05-05 01:14:23 -0400

    fix markdown plugin options for cross-platform compatibility

--- lua/custom/plugins/markdown-preview.lua
+++ lua/custom/plugins/markdown-preview.lua
@@ -2,7 +2,7 @@ return {
   {
     'iamcco/markdown-preview.nvim',
     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    build = 'cd ~/.local/share/nvim/lazy/markdown-preview.nvim && yarn install',
+    build = 'cd app && yarn install',
     init = function()
       vim.g.mkdp_filetypes = { 'markdown' }
     end,
